f4efac5a5a90164048290d6bcf2c131eacf56b69
Gediminas Survila <survila.gediminas@gmail.com>
2019-03-15 10:21:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var items = document.querySelectorAll('.animated');
+
+window.addEventListener('scroll', function(){
+
+	items.forEach(function(item){
+		if (isVisible(item)) {
+			item.classList.add('visible');
+		} else {
+			item.classList.remove('visible');
+		}
+	});
+
+});
+
+function isVisible(el) {
+    const rect = el.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    return vertInView;
+}
